5f9146b2dc7d0a1029e2cf2a5a0e62018c5f7d38
Ehsan <savvyversa@gmail.com>
2026-06-14 00:27:47 +0330

    fix: update site map

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env:{
-    NEXT_PUBLIC_SITE_URL: process?.env?.NEXT_PUBLIC_SITE_URL
+    NEXT_PUBLIC_SITE_URL: process?.env?.NEXT_PUBLIC_SITE_URL,
+    CONTACT_TO_EMAIL: process?.env?.CONTACT_TO_EMAIL,
+    TELEGRAM_BOT_TOKEN: process?.env?.TELEGRAM_BOT_TOKEN,
+    TELEGRAM_CHAT_ID: process?.env?.TELEGRAM_CHAT_ID,
   }
 };
 
